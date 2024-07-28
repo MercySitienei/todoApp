e77046ab936be05910ed5b39bfcb89d4575a0ce5
Mercy Sitienei <sitieneimercy17@gmail.com>
2024-07-28 13:55:26 +0300

    tailwind update

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,16 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./public/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage:{
+      backgroundImage: (theme) => ({
         light_header_image: "url('./public/assets/bg-desktop-light.jpg')",
         dark_header_image: "url('./public/assets/Todo-Desktop.png')",
         light_header_image_mobile: "url('./public/assets/bg-mobile-light.jpg')",
         dark_header_image_mobile: "url('./public/assets/Todo-Mobile.png')",
-      }
+      })
     },
   },
   plugins: [],
